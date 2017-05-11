c82f955a5e770548478c65f9e830fc72d6146128
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2017-05-12 02:03:21 +0900

    curlでpost送信を実装した

--- src/main/java/com/mituba/searcher/SearchEngine.java
+++ src/main/java/com/mituba/searcher/SearchEngine.java
@@ -19,6 +19,11 @@ import com.github.pochi.runner.scripts.ScriptRunnerBuilder;
 
 
 import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 // import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
@@ -119,32 +124,81 @@ public class SearchEngine{
 
     public List<CompareEngine> performSearch(String url) throws IOException, UnsupportedEncodingException{
         try{
-            SolrClient server = new HttpSolrClient.Builder("http://localhost:"+portNum+"/solr/birth_"+kindOfBirthmark+""+coreNum).build();
-            SolrQuery query = createQuery(birthmark, "filename, lev:strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit), data", "2000", "strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit)");
-            // query.set("q", birthmark);
-            // query.set("fl", "filename, lev:strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit), data"); // 返すフィールドの指定
-            // query.set("rows", "2000");
-            // query.setSort("strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit)",SolrQuery.ORDER.desc );
-            QueryResponse response = server.query(query);
-            SolrDocumentList list = response.getResults();
-            List<CompareEngine> CompareEngineList = new ArrayList<>();
-            for(SolrDocument doc : list){
-                if((float)doc.get("lev") <= threshold)
-                    break;
-                CompareEngineList.add(new CompareEngine(filename, birthmark, doc.get("filename").toString(), doc.get("lev").toString(), doc.get("data").toString()));
-                // System.out.println(doc.get("filename") + "," + doc.get("lev") + "," + doc.get("data"));
-            }
-            return CompareEngineList;
+            // SolrClient server = new HttpSolrClient.Builder("http://localhost:"+portNum+"/solr/birth_"+kindOfBirthmark+""+coreNum).build();
+            // SolrQuery query = createQuery(birthmark, "filename, lev:strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit), data", "2000", "strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit)");
+            // QueryResponse response = server.query(query);
+            // SolrDocumentList list = response.getResults();
+            // List<CompareEngine> CompareEngineList = new ArrayList<>();
+            // for(SolrDocument doc : list){
+            //     if((float)doc.get("lev") <= threshold)
+            //         break;
+            //     CompareEngineList.add(new CompareEngine(filename, birthmark, doc.get("filename").toString(), doc.get("lev").toString(), doc.get("data").toString()));
+            //     // System.out.println(doc.get("filename") + "," + doc.get("lev") + "," + doc.get("data"));
+            // }
+            // return CompareEngineList;
+            //
+
+//            String path;
+//            if(coreNum == 0)
+//                path = "http://localhost:"+portNum+"/solr/birth_" + kindOfBirthmark + "/query";
+//            else
+//                path = "http://localhost:"+portNum+"/solr/birth_" + kindOfBirthmark + "" + coreNum + "/query";
+//        	HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead
+//        	try {
+//
+//        	    HttpPost request = new HttpPost(path);
+//        	    StringEntity params =new StringEntity("{"
+//        	    		+ "query:\""+URLEncoder.encode(birthmark, "UTF-8")+"\","
+//        	    		+ "sort:\""+"strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit) desc"+"\","
+//        	    		+ "limit:"+"2010"+","
+//        	    		+ "fields:"+"[\"filename\",\"lev\":strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit),\"data\""+"]"
+//        	    		+ "} ");
+////        	    request.addHeader("content-type", "application/x-www-form-urlencoded");
+//        	    request.setEntity(params);
+//        	    HttpResponse response = httpClient.execute(request);
+//        	    System.out.println("hello");
+//        	    new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
+//        	    	.lines().forEach(System.out::println);
+//        	    return new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
+//        	    	.lines()
+//                    .distinct().parallel()
+//                    .map(i -> i.split(",",3))
+//                    .filter(i -> i.length >= 3  && !Objects.equals(i[1], "lev") && Double.parseDouble(i[1]) >= 0.5)
+//                    .map(n -> new CompareEngine(filename, birthmark, n[0], n[1], n[2]))
+//                    .collect(Collectors.toList());
+//        		Process r = Runtime.getRuntime().exec("curl http://localhost:"+portNum+"/solr/birth_" + kindOfBirthmark + "" + coreNum + "/query?fl=filename,data -d {"
+        		Process r = Runtime.getRuntime().exec("curl http://localhost:"+portNum+"/solr/birth_" + kindOfBirthmark + "" + coreNum + "/query"
+        				// + "?fl=filename,lev:strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8")+"\",edit),data&q="+URLEncoder.encode(birthmark, "UTF-8")+"&sort="+"strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit)+desc"+" -d {"
+        				+ "?fl=filename,lev:strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8")+"\",edit),data&sort="+"strdist(data,\"" + URLEncoder.encode(birthmark, "UTF-8") + "\",edit)+desc"+" -d {"
+        				+ "params:{"
+//        	    		+ "q:"+URLEncoder.encode(birthmark, "UTF-8")+","
+        	    		+ "q:\""+birthmark.replace(" ", "+")+"\","
+        	    		+ "rows:"+2010+","
+        	    		+ "wt:\"csv\""
+        	    		+ "}"
+        	    		+ "}");
+
+        		return new BufferedReader(new InputStreamReader(r.getInputStream())).lines()
+        			.map(n -> n.split(",",3))
+                    .filter(i -> i.length >= 3  && !Objects.equals(i[1], "lev") && Double.parseDouble(i[1]) >= 0.75)
+                    .map(n -> new CompareEngine(filename, birthmark, n[0], n[1], n[2]))
+                    .collect(Collectors.toList());
+
+//        	}catch (Exception ex) {
+//        		return null;
+//        	    //handle exception here
+//
+//        	}
+//        return new BufferedReader(new InputStreamReader(((HttpURLConnection) new URL(url).openConnection()).getInputStream())).lines()
+//            .distinct().parallel()
+//            .map(i -> i.split(",",3))
+//            .filter(i -> i.length >= 3  && !Objects.equals(i[1], "lev") && Double.parseDouble(i[1]) >= 0.5)
+//            .map(n -> new CompareEngine(filename, birthmark, n[0], n[1], n[2]))
+//            .collect(Collectors.toList());
         }catch(Exception e){
             System.out.println(e + ":solrj");
             List<CompareEngine> tmpList = new ArrayList<>();
             return tmpList;
         }
-        // return new BufferedReader(new InputStreamReader(((HttpURLConnection) new URL(url).openConnection()).getInputStream())).lines()
-        //     .distinct().parallel()
-        //     .map(i -> i.split(",",3))
-        //     .filter(i -> i.length >= 3  && !Objects.equals(i[1], "lev") && Double.parseDouble(i[1]) >= 0.5)
-        //     .map(n -> new CompareEngine(filename, birthmark, n[0], n[1], n[2]))
-        //     .collect(Collectors.toList());
     }
 }
